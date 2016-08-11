44be6efdf919322ac3baadf09d1c73bbf116c575
jlatouf2 <jlatouf2@gmail.com>
2016-08-11 18:10:38 -0400

    First

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+/*
+//TO ADD A CLIENT FOLDER AND MAKE IT WORK:
+
+app.use(express.static(path.join(__dirname, '/client')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/client/index.html'));
+});
+
+*/
+app.use(express.static(path.join(__dirname, '/')));
+app.use(express.static(path.join(__dirname, '/white')));
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname,  'white/white.html'));
+});
+
+
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port 5000!');
+});
